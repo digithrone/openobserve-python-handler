8b5dbb523ee79e5d0ebec8475ad80a672ce174cd
Digitrone <info@digithrone.com>
2024-02-17 10:19:02 +0100

    fixed action, bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="openobserve-python-handler",
-    version="0.9.3",
+    version="0.9.4",
     description="Logging handler to send logs to your OpenObserver service",
     keywords="logging handler",
     author="digithrone",
